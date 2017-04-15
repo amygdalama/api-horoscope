ad75de10f0d0e6e68c62cbb2bce4224939a2042b
Amy Hanlon <amyehanlon@gmail.com>
2017-04-15 16:11:41 -0700

    use random wikipedia title

--- horoscope.rb
+++ horoscope.rb
@@ -5,6 +5,9 @@ require 'oauth'
 require 'sinatra'
 
 get '/' do
-  @prediction = ['🍰', '💔', '💪', '🏆', '🐷', '🍕', '🤖'].sample
+  url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json'
+  response = HTTParty.get(url)
+  title = response['query']['random'][0]['title']
+  @prediction = title
   erb :index
 end
