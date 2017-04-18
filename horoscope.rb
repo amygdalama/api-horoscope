9e55d7c0f6e89e481306bfeb5d16e9129145a78f
Amy Hanlon <amyehanlon@gmail.com>
2017-04-18 16:38:13 -0700

    add link to wiki

--- horoscope.rb
+++ horoscope.rb
@@ -8,6 +8,8 @@ get '/' do
   url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json'
   response = HTTParty.get(url)
   title = response['query']['random'][0]['title']
-  @prediction = title
+  page_id = response['query']['random'][0]['id']
+  @wiki_url = "https://en.wikipedia.org/?curid=#{page_id}"
+  @prediction = "You will have a day full of #{title}!"
   erb :index
 end
