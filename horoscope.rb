2a4746afe3680e7f8acac3120d48417a2cb06106
Amy Hanlon <amyehanlon@gmail.com>
2017-04-18 16:38:41 -0700

    add button to post to twitter

--- horoscope.rb
+++ horoscope.rb
@@ -13,3 +13,20 @@ get '/' do
   @prediction = "You will have a day full of #{title}!"
   erb :index
 end
+
+post '/tweet' do
+  prediction = params['prediction']
+  wiki_url = params['wiki_url']
+  puts "wiki url!!!" + wiki_url
+  status = "#{prediction} #{wiki_url}"
+  consumer = OAuth::Consumer.new(
+    ENV['API_KEY'],
+    ENV['API_SECRET'],
+    { site: 'https://api.twitter.com', scheme: 'header' }
+  )
+  token_hash = { oauth_token: ENV['ACCESS_TOKEN'], oauth_token_secret: ENV['ACCESS_TOKEN_SECRET'] }
+  access_token = OAuth::AccessToken.from_hash(consumer, token_hash)
+  response = access_token.request(:post, 'https://api.twitter.com/1.1/statuses/update.json', status: status)
+  puts response
+  redirect to('/')
+end
